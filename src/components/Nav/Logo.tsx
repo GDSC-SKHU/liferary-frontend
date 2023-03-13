4e9c7c347787af875e2640a8817ef753271cb059
Lee2Eunji <eunjijjang11@naver.com>
2023-03-13 18:07:06 +0900

    docs: 로고 변경 및 리펙토링

--- src/components/Nav/Logo.tsx
+++ src/components/Nav/Logo.tsx
@@ -2,22 +2,22 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export default function Logo() {
-    return (
-        <>
-            <Link href={"/"}>
-                <StyledImg src="/Logo.svg" />
-            </Link>
-        </>
-    )
+  return (
+    <>
+      <Link href={"/"}>
+        <StyledImg src="/Logo2.svg" />
+      </Link>
+    </>
+  );
 }
 
 const StyledImg = styled.img`
-width: 18vw;
-height: auto;
+  width: 18vw;
+  height: auto;
 
-margin-top: 3.7vh;
-margin-left: 3vw;
-margin-right: 3vw;
+  margin-top: 3.7vh;
+  margin-left: 3vw;
+  margin-right: 3vw;
 
-display: inline-block;
+  display: inline-block;
 `;
