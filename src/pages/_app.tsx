df5f8a2ea38e58667b00eb6db15290ffa2230d33
Lee2Eunji <eunjijjang11@naver.com>
2023-03-30 17:56:42 +0900

    docs: favico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import GlobalStyle from 'styles/GlobalStyle'
-import type { AppProps } from 'next/app'
+import GlobalStyle from "styles/GlobalStyle";
+import Seo from "@/components/Seo";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) { // Component와 pageProps의 타입은 AppProps다
+export default function App({ Component, pageProps }: AppProps) {
+  // Component와 pageProps의 타입은 AppProps다
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} /> {/* pageProps의 배열을 전부 가져오겠다(깊은 복사) */}
+      <Seo />
+      <Component {...pageProps} />{" "}
+      {/* pageProps의 배열을 전부 가져오겠다(깊은 복사) */}
     </>
-  )
+  );
 }
